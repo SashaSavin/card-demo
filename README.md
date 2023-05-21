0886ae3d0a28af7045ed174150b3d0e4807098c1
sashasavin <praise1234@mail.ru>
2023-05-21 18:15:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Deploy to Github Pages](https://github.com/SashaSavin/card-demo/actions/workflows/deploy.yml/badge.svg)](https://github.com/SashaSavin/card-demo/actions/workflows/deploy.yml)
---
+++ src/components/Card/utils/index.ts
@@ -0,0 +1,22 @@
+interface Colors {
+  [key: string]: string
+}
+
+export const colorObj: Colors = {
+  red: "#EE4444",
+  blue: "#3C81F6",
+  black: "black",
+}
+
+export const getImageByColor = (color: string) => {
+  switch (color) {
+    case colorObj.red:
+      return "images/image_red.png"
+    case colorObj.blue:
+      return "images/image_blue.png"
+    case colorObj.black:
+      return "images/image_black.png"
+    default:
+      return ""
+  }
+}
