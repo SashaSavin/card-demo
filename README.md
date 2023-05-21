629cf7d6934fd5ef857e5e4c718becae534736ea
SashaSavin <SashaSavin@users.noreply.github.com>
2023-05-21 14:31:39 +0000

    deploy: c51655c077fb91e16efb7674f36802178def461d

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Deploy to Github Pages](https://github.com/SashaSavin/card-demo/actions/workflows/deploy.yml/badge.svg)](https://github.com/SashaSavin/card-demo/actions/workflows/deploy.yml)
--- src/shared/api/mock.tsx
+++
@@ -1,9 +0,0 @@
-export const data = [
-  {
-    name: "Zebronics head phone",
-    price: 100,
-    descr:
-      "A portable headphone with a battery life of 20 hours and IP67 rating. Comes with a 3 years warranty.",
-    colors: ["red", "blue", "black"],
-  },
-]
