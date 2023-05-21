abcce632c049b84b104c596868b4769179baad6f
sashasavin <praise1234@mail.ru>
2023-05-21 15:14:06 +0300

    dpl upd

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 
 // const basePath = "/card-demo"
 
+const basePath = process.env.NODE_ENV === "production"
+
 const nextConfig = {
   // basePath,
-  // assetPrefix: `${basePath}/`,
+  assetPrefix: basePath ? "/card-demo/" : "",
   reactStrictMode: true,
   // output: "export",
   images: { unoptimized: true },
